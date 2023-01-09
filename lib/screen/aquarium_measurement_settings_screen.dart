c3fbba37a2cff64611f307368a1657edeb93c3f6
Bryan PROLONG <smyllet.gamer@gmail.com>
2023-01-09 23:53:49 +0100

    [REFACTOR] Add material 3 to aquarium measurement settings screen

--- lib/screen/aquarium_measurement_settings_screen.dart
+++ lib/screen/aquarium_measurement_settings_screen.dart
@@ -14,73 +14,65 @@ class AquariumMeasurementSettingsScreen extends StatefulWidget {
 }
 
 class _AquariumMeasurementSettingsScreenState extends State<AquariumMeasurementSettingsScreen> {
+  late List<MeasurementSettingsModel> measurementSettings;
+
+  @override
+  void initState() {
+    measurementSettings = widget.aquarium.measurementSettingsBloc.getMeasurementSettings();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Theme.of(context).backgroundColor,
-      resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        backgroundColor: Theme.of(context).backgroundColor,
-        elevation: 0,
+        centerTitle: false,
         leading: IconButton(
           icon: const Icon(Icons.arrow_back_rounded),
           onPressed: () => Navigator.pop(context),
         ),
-        title: const Text('Préférences'),
+        title: const Text('Préférences de mesure'),
+        actions: [
+          TextButton(
+            onPressed: () => {
+              widget.aquarium.measurementSettingsBloc.update(measurementSettings),
+              Navigator.pop(context)
+            },
+            child: const Text('Enregistrer'),
+          )
+        ],
       ),
-      body: StreamBuilder<List<MeasurementSettingsModel>>(
-        stream: widget.aquarium.measurementSettingsBloc.stream,
-        builder: (context, snapshot) {
-          if(snapshot.hasData && snapshot.data != null) {
-            if(snapshot.data!.isEmpty) {
-              return const Center(child: Text('Aucune mesures disponibles'));
-            } else {
-              snapshot.data!.sort((a, b) => a.order.compareTo(b.order));
-              return Column(
-                children: [
-                  Expanded(
-                    child: ReorderableListView(
-                      children: snapshot.data!.map((setting) => MeasurementSettingsCard(key : Key(setting.id), measurementSettings: setting)).toList(),
-                      onReorder: (int start, int current) {
-                        for(int i = 0; i < snapshot.data!.length; i++) {
-                          if(snapshot.data![i].order == start) {
-                            snapshot.data![i].order = current;
-                            if(current > start) snapshot.data![i].order--;
-                          } else {
-                            if(current > start && i < current && i > start) {
-                              snapshot.data![i].order--;
-                            } else if(current < start && i >= current && i < start) {
-                              snapshot.data![i].order++;
-                            }
-                          }
-                        }
-
-                        setState(() {});
-                      },
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: ActionButton(
-                      text: 'Enregistrer',
-                      icon: Icons.save,
-                      width: 130,
-                      onPressed: () {
-                        widget.aquarium.measurementSettingsBloc.update(snapshot.data!);
-                        Navigator.pop(context);
+      body: (measurementSettings.isEmpty) ?
+        const Center(
+          child: Text('Aucune préférence de mesure'),
+        ) :
+        Column(
+          children: [
+            Expanded(
+              child: ReorderableListView(
+                children: measurementSettings.map((setting) => MeasurementSettingsCard(key : Key(setting.id), measurementSettings: setting)).toList(),
+                onReorder: (int start, int current) {
+                  for(int i = 0; i < measurementSettings.length; i++) {
+                    if(measurementSettings[i].order == start) {
+                      measurementSettings[i].order = current;
+                      if(current > start) measurementSettings[i].order--;
+                    } else {
+                      if(current > start && i < current && i > start) {
+                        measurementSettings[i].order--;
+                      } else if(current < start && i >= current && i < start) {
+                        measurementSettings[i].order++;
                       }
-                    ),
-                  )
-                ],
-              );
-            }
-          } else {
-            return const Center(
-              child: CircularProgressIndicator(),
-            );
-          }
-        },
-      ),
+                    }
+                  }
+
+                  setState(() {
+                    measurementSettings.sort((a, b) => a.order.compareTo(b.order));
+                  });
+                },
+              ),
+            ),
+          ],
+        ),
     );
   }
 }

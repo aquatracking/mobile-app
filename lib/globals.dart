46496d93db3e792ba12a0db43c531ba15cd63d4c
Bryan PROLONG <smyllet.gamer@gmail.com>
2023-01-11 13:36:27 +0100

    [ADD] Add debug mode for conntect to localhost

--- lib/globals.dart
+++ lib/globals.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-String apiBaseUrl = 'https://aquatracking.bryanprolong.fr';
+// add this '--dart-define="DEBUG=true"' in additional run args on flutter run for enable debug
+bool debug = const bool.fromEnvironment('DEBUG');
+
+String apiBaseUrl = (debug) ? 'http://localhost:3000' : 'https://aquatracking.bryanprolong.fr';
 ImageProvider imagePlaceholder = Image.asset('assets/images/placeholder.jpg').image;
+Color colorSeed = (debug) ? Colors.green : Colors.cyan;
 
 
 late SharedPreferences prefs;
